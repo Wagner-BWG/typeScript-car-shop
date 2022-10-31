2ef24e9ab531394694c6d84deab53e95d5c448a0
Wagner B <wagner_mbarbosa@hotmail.com>
2022-10-30 22:28:18 -0300

    :bangbang:FEAT: Implementa testes da camada Controller

---
+++ src/tests/unit/controllers/carsController.test.ts
@@ -0,0 +1,35 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import { NextFunction, Request, Response } from 'express';
+import CarsModel from '../../../models/CarsModel';
+import CarsService from '../../../services/CarsService';
+import CarsController from '../../../controllers/CarsController';
+import { carMock, carMockWithId, carMock2, carMock2WithId1, carMockArray } from '../mocks/carMock';
+
+describe('Cars Controller', () => {
+  const carsModel = new CarsModel();
+  const carsService = new CarsService(carsModel);
+  const carsController = new CarsController(carsService);
+  const req = {} as Request;
+  const res = {} as Response;
+
+  before(async () => {
+    sinon
+      .stub(carsService, 'create')
+      .resolves(carMock);
+  });
+
+  after(()=>{
+    sinon.restore();
+  })
+
+  describe('Creating an entry', () => {
+    it('Success', async () => {
+      req.body = carMock;
+      await carsController.create(req, res);
+      expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(carMock)).to.be.true;
+    })
+  })
+
+});
